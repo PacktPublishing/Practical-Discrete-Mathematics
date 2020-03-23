0ee82676d59f54a59a876ac6c45fad9378bfa5ee
rtwhite1546 <ryan@chasnet.com>
2020-03-22 21:07:44 -0400

    Update NetflixUsersExample.py

--- Chapter 3/NetflixUsersExample.py
+++ Chapter 3/NetflixUsersExample.py
@@ -1,14 +1,20 @@
+# Import the pandas library
 import pandas as pd
 
+# Read the provided customer data CSV file
 customer_df = pd.read_csv("C:/Users/anipb/Desktop/CustomerList.csv")
 
+# Display the customer data
 print(customer_df)
 
+# Find customers from Georgia and from the USA
 print("Example for AND operator")
 print(customer_df.loc[(customer_df['Country'] == 'USA') & (customer_df['State'] == 'Georgia')])
 
+# Find customers from the USA or from Ontario
 print("Example for OR operator")
 print(customer_df.loc[(customer_df['Country'] == 'USA') | (customer_df['State'] == 'Ontario')])
 
+# Find customers who are not from the USA
 print("Example for NOT operator")
 print(customer_df.loc[(customer_df['Country'] != 'USA')])
