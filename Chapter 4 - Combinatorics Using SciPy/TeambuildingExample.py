5e28b6707fabb08644010e5da44bcc97adca911a
rtwhite1546 <ryan@chasnet.com>
2020-12-30 00:23:06 -0500

    Finalizing Ch 3, 4, 5, 8

--- Chapter 4 - Combinatorics Using SciPy/TeambuildingExample.py
+++
@@ -1,7 +0,0 @@
-# using the factorial function
-import math
-print(math.factorial(20) // math.factorial(4) // math.factorial(20-4))
-
-# import the special functions from sciPy
-import scipy.special
-print(scipy.special.binom(20,4))
